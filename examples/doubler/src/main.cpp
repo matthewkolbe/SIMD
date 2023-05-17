34e016594493e62af3565452d3e30a585026c75e
matthew <matthewkolbe@gmail.com>
2023-05-17 15:37:22 -0600

    doubler example

---
+++ examples/doubler/src/main.cpp
@@ -0,0 +1,72 @@
+
+#include <immintrin.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include"../../../src/simd_unroller.hh"
+
+
+struct doubleit {
+    static void func(__m512d x, __m512d & y) {
+        y = _mm512_add_pd(x, x);
+    }
+
+    static void maskfunc(__m512d x, const unsigned int size, __m512d & y) {
+        unsigned char mask = 0xFF << size;
+        y = _mm512_maskz_add_pd(~mask, x, x);
+    }
+};
+
+struct load {
+    static __m512d func(double*from) {
+        return _mm512_loadu_pd(from);
+    }
+
+    static __m512d maskfunc(double*from, const unsigned int size) {
+        unsigned char mask = 0xFF << size;
+        return _mm512_maskz_loadu_pd(~mask, from);
+    }
+};
+
+
+struct store {
+    static void func(__m512d x, double*to) {
+        _mm512_storeu_pd(to, x);
+    }
+
+    static void maskfunc(__m512d x, const unsigned int size, double*to) {
+        unsigned char mask = 0xFF << size;
+        return _mm512_mask_storeu_pd(to, ~mask, x);
+    }
+};
+
+struct reduce {
+    constexpr static bool is_valid() {
+        return false;
+    }
+
+    static void func(__m512d x, double*to) {
+        
+    }
+
+    static void func(__m512d x0, __m512d x1, __m512d x2, __m512d& y) {
+        
+    }
+};
+
+
+int main() {
+    const int N = 35128;
+    auto n = rand() % N;
+    double x[n];
+    double y[n];
+    for (int i = 0; i < n; ++i)
+        x[i] = (double)i;
+
+    unroller<double, double, __m512d, __m512d, 
+             doubleit, load, store, reduce>(x, y, n);
+
+    auto index = rand() % n;
+    std::cout << index << "*2 = " << y[index] << std::endl;
+    std::cout << n-1 << "*2 = " << y[n-1] << std::endl;
+}
